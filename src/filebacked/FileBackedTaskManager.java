c4d5438274051c0099ccf363889f832d7875b63a
Sergey <Lihachev.S.71y@yandex.ru>
2025-02-04 19:54:52 +0300

    Коммит 21.Создал класс FileBackedTaskManagerTest. Реализовал тесты на основной функционал менеджера FileBackedTaskManager. В FileBackedTaskManager добавил(переопределил) основные методы класса наследника (которые изменяют состояние задач).

--- src/filebacked/FileBackedTaskManager.java
+++ src/filebacked/FileBackedTaskManager.java
@@ -23,44 +23,6 @@ public class FileBackedTaskManager extends InMemoryTaskManager {
         this.path = path;
     }
 
-    //Данный метод возвращает менеджер с загруженной информацией о задачах в оперативную память из ссылки
-    public static FileBackedTaskManager loadFromFile(Path path) {
-        FileBackedTaskManager manager = new FileBackedTaskManager(path);
-        List<String> lines = new ArrayList<>();
-        try {
-            String fileContent = Files.readString(path, StandardCharsets.UTF_8);
-            String[] words = fileContent.split("\n");
-            lines.addAll(Arrays.asList(words));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        for (String line : lines) {
-            Task task = taskFromString(line);
-            if (task.getId() > manager.nextId) {
-                manager.nextId ++;
-            }
-            if (task.getType() == TypesOfTasks.TASK) {
-                manager.simpleTasks.put(task.getId(), task);
-            } else if (task.getType() == TypesOfTasks.EPIC) {
-                manager.epics.put(task.getId(), (Epic) task);
-            } else if (task.getType() == TypesOfTasks.SUBTASK) {
-                manager.subtasks.put(task.getId(), (Subtask) task);
-            }
-            //теперь нужно в каждый эпик добавить айдишники соответствующих ему сабтасков,
-            //так как эту информацию мы не записывали, но её можно восстановить из сабтасков (они хранят айдишники своих эпиков)
-            for (Subtask subtask : manager.subtasks.values()) {
-                manager.epics.get(subtask.getEpicId()).addSubtaskId(subtask.getId());
-            }
-        }
-        return manager;
-    }
-
-    @Override
-    public void add(Task simpleTask) {
-        super.add(simpleTask);
-        save();
-    }
-
     private void save() {
         // Формируем общий список ключей (идентификаторов)
         List<Integer> keys = new ArrayList<>();
@@ -88,10 +50,10 @@ public class FileBackedTaskManager extends InMemoryTaskManager {
             try {
                 writer.write("id,type,name,status,description,epic");
                 writer.newLine();
-                    for (Task task : result) {
-                        writer.write(taskToString(task));
-                        writer.newLine();
-                    }
+                for (Task task : result) {
+                    writer.write(taskToString(task));
+                    writer.newLine();
+                }
             } catch (IOException e) {
                 throw new ManagerSaveException("Ошибка при записи в файл");
             }
@@ -126,36 +88,139 @@ public class FileBackedTaskManager extends InMemoryTaskManager {
         String description;
         int epicId;
         String[] parts = taskAsString.split(",");
-            switch (parts[1]) {
-                case "TASK":
-                    id = Integer.parseInt(parts[0]);
-                    title = parts[2];
-                    status = Status.valueOf(parts[3]);
-                    description = parts[4];
-                    task = new Task(title, description, id, status);
-                    break;
-                case "EPIC":
-                    id = Integer.parseInt(parts[0]);
-                    title = parts[2];
-                    status = Status.valueOf(parts[3]);
-                    description = parts[4];
-                    task = new Epic(title, description, id, status);
-                    break;
-                case "SUBTASK":
-                    id = Integer.parseInt(parts[0]);
-                    title = parts[2];
-                    status = Status.valueOf(parts[3]);
-                    description = parts[4];
-                    epicId = Integer.parseInt(parts[5]);
-                    task = new Subtask(title, description, id, status, epicId);
-                    break;
-                default:
-                    throw new IllegalStateException("Unexpected value: " + parts[1]);
-            }
-            
+        switch (parts[1]) {
+            case "TASK":
+                id = Integer.parseInt(parts[0]);
+                title = parts[2];
+                status = Status.valueOf(parts[3]);
+                description = parts[4];
+                task = new Task(title, description, id, status);
+                break;
+            case "EPIC":
+                id = Integer.parseInt(parts[0]);
+                title = parts[2];
+                status = Status.valueOf(parts[3]);
+                description = parts[4];
+                task = new Epic(title, description, id, status);
+                break;
+            case "SUBTASK":
+                id = Integer.parseInt(parts[0]);
+                title = parts[2];
+                status = Status.valueOf(parts[3]);
+                description = parts[4];
+                epicId = Integer.parseInt(parts[5]);
+                task = new Subtask(title, description, id, status, epicId);
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + parts[1]);
+        }
+
         return task;
     }
 
+    //Данный метод возвращает менеджер с загруженной информацией о задачах в оперативную память из ссылки
+    public static FileBackedTaskManager loadFromFile(Path path) {
+        FileBackedTaskManager manager = new FileBackedTaskManager(path);
+        List<String> lines = new ArrayList<>();
+        try {
+            String fileContent = Files.readString(path, StandardCharsets.UTF_8);
+            String[] words = fileContent.split("\n");
+            lines.addAll(Arrays.asList(words));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        for (String line : lines) {
+            Task task = taskFromString(line);
+            if (task.getId() > manager.nextId) {
+                manager.nextId++;
+            }
+            if (task.getType() == TypesOfTasks.TASK) {
+                manager.simpleTasks.put(task.getId(), task);
+            } else if (task.getType() == TypesOfTasks.EPIC) {
+                manager.epics.put(task.getId(), (Epic) task);
+            } else if (task.getType() == TypesOfTasks.SUBTASK) {
+                manager.subtasks.put(task.getId(), (Subtask) task);
+            }
+            //теперь нужно в каждый эпик добавить айдишники соответствующих ему сабтасков,
+            //так как эту информацию мы не записывали, но её можно восстановить из сабтасков (они хранят айдишники своих эпиков)
+            for (Subtask subtask : manager.subtasks.values()) {
+                manager.epics.get(subtask.getEpicId()).addSubtaskId(subtask.getId());
+            }
+        }
+        return manager;
+    }
+
+    @Override
+    public void add(Task simpleTask) {
+        super.add(simpleTask);
+        save();
+    }
+
+    @Override
+    public void add(Epic epic) {
+        super.add(epic);
+        save();
+    }
+
+    @Override
+    public void add(Subtask subtask) {
+        super.add(subtask);
+        save();
+    }
+
+    @Override
+    public void update(Task simpleTask) {
+        super.update(simpleTask);
+        save();
+    }
+
+    @Override
+    public void update(Epic epic) {
+        super.update(epic);
+        save();
+    }
+
+    @Override
+    public void update(Subtask subtask) {
+        super.update(subtask);
+        save();
+    }
+
+    @Override
+    public void deleteAllTasks() {
+        super.deleteAllTasks();
+        save();
+    }
+
+    @Override
+    public void deleteAllSubtasks() {
+        super.deleteAllSubtasks();
+        save();
+    }
+
+    @Override
+    public void deleteAllEpics() {
+        super.deleteAllEpics();
+        save();
+    }
+
+    @Override
+    public void deleteTaskById(int id) {
+        super.deleteTaskById(id);
+        save();
+    }
+
+    @Override
+    public void deleteSubtaskById(int id) {
+        super.deleteSubtaskById(id);
+        save();
+    }
+
+    @Override
+    public void deleteEpicById(int id) {
+        super.deleteEpicById(id);
+        save();
+    }
 }
 
 
--- test/FileBackedTaskManagerTest.java
+++ test/FileBackedTaskManagerTest.java
@@ -1,19 +1,130 @@
 import enams.Status;
 import filebacked.FileBackedTaskManager;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import storage.InMemoryTaskManager;
 import tasks.Epic;
 import tasks.Subtask;
 import tasks.Task;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class FileBackedTaskManagerTest extends InMemoryTaskManagerTest{
+class FileBackedTaskManagerTest extends InMemoryTaskManagerTest {
+
     @Override
+    @BeforeEach
     void BeforeEach() {
-        taskManager = new FileBackedTaskManager();
+        Path tempFile = null;
+        try {
+            tempFile = Files.createTempFile(null, ".txt");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        tempFile.toFile().deleteOnExit();
+        taskManager = new FileBackedTaskManager(tempFile);
         simpleTask1 = new Task("Простая задача 1", "Описание простой задачи 1", 1, Status.NEW);
         epic1 = new Epic("Эпик 1", "Описание эпика 1", 1, Status.NEW);
         subtask1OfEpic1 = new Subtask("Подзадача 1, эпика 1", "Описание подзадачи 1, эпика 1",
                 1, Status.NEW, 2);
+    }
+
+    @Test
+    @Override
+    void testAdd() {
+        super.testAdd();
+    }
+
+    @Test
+    @Override
+    void testUpdate() {
+        super.testUpdate();
+    }
+
+    @Test
+    @Override
+    void testGetSimpleTasks() {
+        super.testGetSimpleTasks();
+    }
+
+    @Test
+    @Override
+    void testGetSubtasks() {
+        super.testGetSubtasks();
+    }
+
+    @Test
+    @Override
+    void testGetEpics() {
+        super.testGetEpics();
+    }
+
+    @Test
+    @Override
+    void deleteAllTasks() {
+        super.deleteAllTasks();
+    }
+
+    @Test
+    @Override
+    void deleteAllSubtasks() {
+        super.deleteAllSubtasks();
+    }
+
+    @Test
+    @Override
+    void deleteAllEpics() {
+        super.deleteAllEpics();
+    }
+
+    @Test
+    @Override
+    void getSimpleTaskById() {
+        super.getSimpleTaskById();
+    }
+
+    @Test
+    @Override
+    void getSubtaskById() {
+        super.getSubtaskById();
+    }
+
+    @Test
+    @Override
+    void getEpicById() {
+        super.getEpicById();
+    }
+
+    @Test
+    @Override
+    void deleteTaskById() {
+        super.deleteTaskById();
+    }
+
+    @Test
+    @Override
+    void deleteSubtaskById() {
+        super.deleteSubtaskById();
+    }
+
+    @Test
+    @Override
+    void deleteEpicById() {
+        super.deleteEpicById();
+    }
+
+    @Test
+    @Override
+    void getSubtasksOfEpic() {
+        super.getSubtasksOfEpic();
+    }
+
+    @Test
+    @Override
+    void getHistory() {
+        super.getHistory();
     }
 }
